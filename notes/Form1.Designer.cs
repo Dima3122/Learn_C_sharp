8acc891d715fd8e36e4bc8817478e8d5cfe4d14e
Дмитрий Савостин <dimasavostin075@gmail.com>
2021-04-15 07:42:03 +0700

    Learn C#

---
+++ notes/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace notes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.менюToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.оПрограммеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.удалитьВсеЗаметкиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 31);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(468, 40);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(486, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 40);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Добавить заметку";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataGridView);
+            this.groupBox1.Location = new System.Drawing.Point(12, 87);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(776, 369);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Заметки";
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(6, 21);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowHeadersWidth = 51;
+            this.dataGridView.RowTemplate.Height = 24;
+            this.dataGridView.Size = new System.Drawing.Size(764, 342);
+            this.dataGridView.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(640, 31);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(148, 40);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Удалить заметку";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.менюToolStripMenuItem,
+            this.оПрограммеToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // менюToolStripMenuItem
+            // 
+            this.менюToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.сохранитьToolStripMenuItem,
+            this.удалитьВсеЗаметкиToolStripMenuItem});
+            this.менюToolStripMenuItem.Name = "менюToolStripMenuItem";
+            this.менюToolStripMenuItem.Size = new System.Drawing.Size(65, 24);
+            this.менюToolStripMenuItem.Text = "Меню";
+            // 
+            // оПрограммеToolStripMenuItem
+            // 
+            this.оПрограммеToolStripMenuItem.Name = "оПрограммеToolStripMenuItem";
+            this.оПрограммеToolStripMenuItem.Size = new System.Drawing.Size(118, 24);
+            this.оПрограммеToolStripMenuItem.Text = "О программе";
+            this.оПрограммеToolStripMenuItem.Click += new System.EventHandler(this.оПрограммеToolStripMenuItem_Click_1);
+            // 
+            // сохранитьToolStripMenuItem
+            // 
+            this.сохранитьToolStripMenuItem.Name = "сохранитьToolStripMenuItem";
+            this.сохранитьToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.сохранитьToolStripMenuItem.Text = "Сохранить";
+            this.сохранитьToolStripMenuItem.Click += new System.EventHandler(this.сохранитьToolStripMenuItem_Click);
+            // 
+            // удалитьВсеЗаметкиToolStripMenuItem
+            // 
+            this.удалитьВсеЗаметкиToolStripMenuItem.Name = "удалитьВсеЗаметкиToolStripMenuItem";
+            this.удалитьВсеЗаметкиToolStripMenuItem.Size = new System.Drawing.Size(235, 26);
+            this.удалитьВсеЗаметкиToolStripMenuItem.Text = "Удалить все заметки";
+            this.удалитьВсеЗаметкиToolStripMenuItem.Click += new System.EventHandler(this.удалитьВсеЗаметкиToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 468);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Заметки";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem менюToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem оПрограммеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem удалитьВсеЗаметкиToolStripMenuItem;
+    }
+}
+
---
+++ notes/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace notes
+{
+    public partial class Form1 : Form
+    {
+        int count = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            HeaderOfTheTable();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            count++;
+            dataGridView.Rows.Add(textBox1.Text, DateTime.Now);
+            textBox1.Text = "";
+        }
+        public void HeaderOfTheTable()
+        {
+            var colomn1 = new DataGridViewColumn();
+            colomn1.HeaderText = "Данные";
+            colomn1.Width = 405;
+            colomn1.CellTemplate = new DataGridViewTextBoxCell();
+            colomn1.Name = "data";
+            colomn1.Frozen = true;
+
+            var colomn2 = new DataGridViewColumn();
+            colomn2.HeaderText = "Время";
+            colomn2.Width = 125;
+            colomn2.CellTemplate = new DataGridViewTextBoxCell();
+            colomn2.Name = "time";
+            colomn2.Frozen = true;
+
+            dataGridView.Columns.Add(colomn1);
+            dataGridView.Columns.Add(colomn2);
+            dataGridView.AllowUserToAddRows = false;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (count == 0)
+            {
+                MessageBox.Show("Список дел пуст!");
+                return;
+            }
+            int ind = dataGridView.SelectedCells[0].RowIndex;
+            dataGridView.Rows.RemoveAt(ind);
+            count--;
+        }
+        private void оПрограммеToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("Создатель Di");
+        }
+
+        private void сохранитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FileStream fs = new FileStream(@"D:\програмирование\программы\Изучение C#\калькулятор на C#\calculator\notes\data\1.txt", FileMode.Create);
+            StreamWriter streamWriter = new StreamWriter(fs);//записывальщик в файл
+            try
+            {
+                for (int j = 0; j < dataGridView.Rows.Count; j++)
+                {
+                    for (int i = 0; i < dataGridView.Rows[j].Cells.Count; i++)
+                    {
+                        streamWriter.Write(dataGridView.Rows[j].Cells[i].Value + "/");
+                    }
+
+                    streamWriter.WriteLine();
+                }
+                streamWriter.Write("{" + count);
+                streamWriter.Close();
+                fs.Close();
+
+                MessageBox.Show("Файл успешно сохранен");
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка при сохранении файла!");
+            }
+        }
+        private void удалитьВсеЗаметкиToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            dataGridView.Rows.Clear();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FileStream fs = new FileStream(@"D:\програмирование\программы\Изучение C#\калькулятор на C#\calculator\notes\data\1.txt", FileMode.Open);
+            StreamReader streamReader = new StreamReader(fs);
+            string str = streamReader.ReadToEnd();
+            char [] splitsymbol1 = { '\n' };
+            string [] str2 = str.Split(splitsymbol1);
+            int count_notes = str2.Length - 1;
+            string [] str3;
+            char[] splitsymbol2 = { '/' };
+            char[] splitsymbol3 = { '{' };
+            for (int i = 0; i < count_notes; i++)
+            {
+                str3 = str2[i].Split(splitsymbol2);
+                dataGridView.Rows.Add(str3[0], str3[1]);
+            }
+            str3 = str.Split(splitsymbol3);
+            int count3 = str3.Length;
+            count = Convert.ToInt32(str3[count3-1]);
+            streamReader.Close();
+        }
+    }
+}
